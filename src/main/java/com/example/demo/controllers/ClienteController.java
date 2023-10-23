3b248441635f2d6925d8644eb64880819fa381d7
lctiago <lucas.pereira@everymind.com.br>
2023-10-23 16:40:22 -0300

    Adiciona novo endpoint

--- src/main/java/com/example/demo/controllers/ClienteController.java
+++ src/main/java/com/example/demo/controllers/ClienteController.java
@@ -1,11 +1,17 @@
 package com.example.demo.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,5 +36,19 @@ public class ClienteController {
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> atualizar(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Optional<Cliente> clienteOpt = clienteRepository.findById(id);
+		if (clienteOpt.isEmpty()) {
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.body(null);
+		}
+		var clienteModel = clienteOpt.get();
+		BeanUtils.copyProperties(cliente, clienteModel);
+		clienteModel.setId(id);
+		return ResponseEntity.status(HttpStatus.OK).body(clienteRepository.save(clienteModel));
 	}
 }
